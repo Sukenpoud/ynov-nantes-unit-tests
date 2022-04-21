0111b1ae4a4f682ac39a8a52d6f61823ef186583
Benjamin Cron <benjamin.cron.17@gmail.com>
2022-04-21 15:03:44 +0200

    update temp tracker

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,31 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEquals(42, 42);
+
+    // Test get_mean method
+    public function test_get_mean() {
+        $tempTracker = new TempTracker;
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_mean(), 50.0);
+    }
+
+    // Test get_min method
+    public function test_get_min() {
+        $tempTracker = new TempTracker;
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_min(), 45.0);
+    }
+
+    // Test get_max method
+    public function test_get_max() {
+        $tempTracker = new TempTracker;
+        $tempTracker->insert(55);
+        $tempTracker->insert(45);
+        $tempTracker->insert(50);
+        $this->assertEquals($tempTracker->get_max(), 55.0);
     }
 }

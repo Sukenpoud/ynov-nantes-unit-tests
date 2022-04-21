1253136388d67b2303b9f70085a43847bc8bb37c
Romain <axilais.romain@gmail.com>
2022-04-21 14:44:43 +0200

    create tests for flatten.php

--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,23 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
-    {
-        $this->assertEquals(1337, 1337);
+
+    // Tests with empty lists
+    public function test_with_null(){
+        $this->assertEquals(flatten([]), [], "Should be []");
+        $this->assertEquals(flatten([[[]]]), [], "Should be []");
     }
+
+    // Tests with a single value in list
+    public function test_single_value(){
+        $this->assertEquals(flatten([5]), [5], "Should be [5]");
+        $this->assertEquals(flatten([-5]), [-5], "Should be [-5]");
+    }
+
+    // Tests with lists nested few times in list
+    public function test_nested_multiple(){
+        $this->assertEquals(flatten([4, [5, [6, 7]], [1, 2, 3]]), [4, 5, 6, 7, 1, 2, 3], "Should be [4, 5, 6, 7, 1, 2, 3]");
+    }
+
+    
 }

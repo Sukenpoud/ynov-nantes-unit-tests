5d51e91b49b9b0b70bae8a1e27f8cc1b6f8d8443
Benjamin Cron <benjamin.cron.17@gmail.com>
2022-04-29 10:23:22 +0200

    test : vérifier que chaque case prend la valeur du nombre de bombes adjacentes

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -29,7 +29,7 @@ Idées tests :
 
 - vérifier que le constructeur crée une map de la bonne taille ( bon nb de ligne 0 < L <= 100 et bon nb de colonne 0 < C <= 100)
 - vérifier que le constructeur crée une map avec le bon nombre de bombe ( nbBombe )
-- 
+- vérifier que chaque case prend la valeur du nombre de bombes adjacentes (8 cases voisines)
 
 */
 
@@ -41,14 +41,13 @@ describe("Minesweeper", function() {
     const lignes = 5;
     const colonnes = 5;
     const mines = 4;
-    const tailleTableau = lignes*colonnes;
 
     // When
     const map = new Map(lignes, colonnes, mines);
     const results = map.createMap().flat().length;
 
     // Then
-    const expected = tailleTableau;
+    const expected = lignes*colonnes;
 
     expect(results).toStrictEqual(expected);
   });
@@ -70,5 +69,30 @@ describe("Minesweeper", function() {
     const expected = mines;
 
     expect(nbMines).toBe(expected);
+  });
+
+  it("vérifier que chaque case prend la valeur du nombre de bombes adjacentes", function() {
+    // Given
+    const map =   [
+        [ '.', '*', '.', '.', '.' ],
+        [ '.', '.', '*', '.', '.' ],
+        [ '.', '*', '.', '.', '.' ],
+        [ '.', '.', '*', '.', '*' ],
+        [ '*', '.', '.', '.', '.' ]
+      ]
+
+    // When
+    const result = map.browseMapForIndex()
+
+    // Then
+    const expected = [
+        [ '1', '*', '2', '1', '0' ],
+        [ '2', '3', '*', '1', '0' ],
+        [ '1', '*', '3', '1', '1' ],
+        [ '2', '3', '*', '2', '*' ],
+        [ '*', '2', '1', '2', '1' ]
+      ]
+
+    expect(result).toBe(expected);
   });
 });
